e2d7c95989cc6e4883754c5eb9504ba2d0144ede
Febrian1202 <febriansyahrian25@gmail.com>
2025-11-24 09:39:24 +0800

    Menyelesaikan laravel 11 wpu tentang Eloquent ORM & Post Model

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -1,40 +1,13 @@
 <?php
+
 namespace App\Models;
 
-use Illuminate\Support\Arr;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
 
-class Post 
+class Post extends Model
 {
-    public static function all()
-    {
-        return [
-            [
-                'id' => 1,
-                'slug' => 'judul-artikel-pertama',
-                "title" => "Judul Artikel Pertama",
-                "author" => "M. Febrian Syah",
-                "body" => "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nostrum sed, voluptas ut similique sequi enim possimus nulla consequuntur, cum eius nemo aliquam officiis eligendi laboriosam at animi obcaecati libero tempore."
-            ],
-            [
-                "id"=> 2,
-                'slug' => 'judul-artikel-kedua',
-                "title" => "Judul Artikel Kedua",
-                "author" => "M. Febrian Syah",
-                "body" => "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nostrum sed, voluptas ut similique sequi enim possimus nulla consequuntur, cum eius nemo aliquam officiis eligendi laboriosam at animi obcaecati libero tempore."
-            ]
-        ];
-    }
-
-    public static function find($slug): array {
-        // return Arr::first(static::all(), function($post) use ($slug) {
-        // return $post['slug'] == $slug;
-
-        $post = Arr::first(static::all(), fn($post) => $post['slug'] == $slug);
-
-        if(! $post) {
-            abort(404);
-        }
-        
-        return $post;
-    }
+    //
+    use HasFactory;
+    protected $fillable = ['title', 'slug', 'author', 'body'];
 }
--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -3,9 +3,9 @@
     <article class="py-8 max-w-screen-md ">
         <h2 class="mb-1 text-3xl tracking-tight font-bold text-gray-200">{{ $post['title'] }}</h2>
         <div class="text-base text-gray-300">
-            <a href="#">{{ $post['author'] }}</a> | 1 January 2024
+            <a href="#">{{ $post['author'] }}</a> | {{ $post->created_at->diffForHumans() }}
         </div>
         <p class="my-4 font-light text-white">{{ $post['body'] }}</p>
-        <a href="/posts" class="font-medium text-blue-300 hover:underline">&laquo;Back to post</a>
+        <a href="/posts" class="font-medium text-blue-300 hover:underline">&laquo; Back to post</a>
     </article>
 </x-layout>

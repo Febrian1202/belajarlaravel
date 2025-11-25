3197b597c2b61523819dd54e3c28a8cfdb09228f
Febrian1202 <febriansyahrian25@gmail.com>
2025-11-25 19:58:15 +0800

    Membuat Seeder

---
+++ database/seeders/CategorySeeder.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Post;
+use App\Models\Category;
+use Illuminate\Database\Seeder;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+class CategorySeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        //
+        Category::create([
+            'name'=> 'Web Development',
+            'slug'=> 'web-development'
+        ]);
+
+        Category::create([
+            'name'=> 'Web Design',
+            'slug'=> 'web-design'
+        ]);
+
+        Category::create([
+            'name'=> 'UI UX',
+            'slug'=> 'ui-ux'
+        ]);
+
+        Category::create([
+            'name'=> 'Machine Learning',
+            'slug'=> 'machine-learning'
+        ]);
+
+        Category::create([
+            'name'=> 'Data Structure & Algorithm',
+            'slug'=> 'data-structure-algorithm'
+        ]);
+    }
+}
